04a4f3cf9e4185f8ca9a994dff05a473695b2c3c
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2020-07-01 23:21:20 +0300

    wip: testing auto tune

--- e2e/jenkins-master/jobs_sys/_new_ci/_new_ci.jdsl.groovy
+++ e2e/jenkins-master/jobs_sys/_new_ci/_new_ci.jdsl.groovy
@@ -2,17 +2,47 @@ job("_new_ci") {
 	description()
 	keepDependencies(false)
 	parameters {
-		choiceParam("jenfile_uri", ["__custom__", "https://github.com/gzvulon/jenlib.git;e2e/data4test/parallel_tasks_jen/build.groovy"], "repo;path_jenkinsfile")
-		stringParam("__custom__", "", "")
+		stringParam("jobname_ci", "jenlib_pipe_master_ci_X", "jenlib_pipe_master_ci")
+		stringParam("github_p", "gzvulon/jenlib", "github_p")
+		stringParam("jengroovy_p", "e2e/data4test/parallel_tasks_jen/build.groovy", "jengroovy_p")
 	}
 	disabled(false)
 	concurrentBuild(false)
 	steps {
-		shell(
-"""
-echo \$jenfile_uri
-echo cus \$__custom__
-""")
+		dsl {
+			text("""// def jobname_ci = '{{.JOB_NAME}}' // jenlib_pipe_master_ci
+// def github_p = '{{.GITHUB_PATH}}' // "gzvulon/jenlib"
+// def jengroovy_p = '{{.JENKINS_FILE_PATH}}' // "e2e/data4test/parallel_tasks_jen/build.groovy"
+
+pipelineJob("\${jobname_ci}") {
+	description()
+	keepDependencies(false)
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						github("\${github_p}", "https")
+					}
+					branch("*/master")
+				}
+			}
+			scriptPath("\${jengroovy_p}")
+		}
+	}
+	disabled(false)
+	configure {
+		it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
+			'autoRebuild'('false')
+			'rebuildDisabled'('false')
+		}
+	}
+}""")
+			ignoreExisting(false)
+			removeAction("IGNORE")
+			removeViewAction("IGNORE")
+			lookupStrategy("JENKINS_ROOT")
+		}
 	}
 	configure {
 		it / 'properties' / 'com.sonyericsson.rebuild.RebuildSettings' {
