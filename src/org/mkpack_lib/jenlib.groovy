f8afed50de2d3e557f0d7d341504858363e7f713
Yair Dar <yairda@mobileye.com>
2020-11-10 04:57:16 +0200

    oh my

--- src/org/mkpack_lib/jenlib.groovy
+++ src/org/mkpack_lib/jenlib.groovy
@@ -75,7 +75,7 @@ def parse_env_file(multiline_text){
 
 // --- public ---
 
-class jenlib {
+class jakelib {
   static def parse_env_file_text(env_file_text) {
     return parse_env_file(env_file_text)
   }
