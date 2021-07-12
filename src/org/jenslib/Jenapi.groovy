40b26bda09fcc279682b3a22d2a79f737c2e4a8e
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2021-07-12 04:55:21 +0200

    fix things

--- src/org/jenslib/Jenapi.groovy
+++ src/org/jenslib/Jenapi.groovy
@@ -1,11 +1,46 @@
 package org.jenslib
 
-class Jenapi{
-    public static String reverse_string(String s){
-        return s.reverse()
-    }
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
 
-    def process_taskfile(text){
-
-    }
+def reverse_string(String s){
+    return s.reverse()
 }
+
+def checkOutFrom(repo) {
+  sh(" echo git@github.com:jenkinsci/${repo}")
+}
+
+def text_yaml_to_map(text){
+    def yaml_parser = new Yaml()
+    def parsed = yaml_parser.load(text)
+    return parsed
+}
+
+def text_yaml_from_map(the_map){
+    def yaml_parser = new Yaml()
+    def text = yaml_parser.dump(the_map)
+    return text
+}
+
+def stages_from_task(task_map){
+  for (cmd in task_map.cmds){
+    stage(cmd){
+      sh(cmd)
+    }
+  }
+}
+
+def stage_with_steps_from_task(Map cfg =[:]){
+  def taskmap = cfg.taskmap; assert taskmap
+  def taskname = cfg.taskname; assert taskname
+  // end of header schema
+
+  stage(taskname){
+    for (cmd in taskmap.cmds){
+        sh(cmd)
+    }
+  }
+}
+
+return this
