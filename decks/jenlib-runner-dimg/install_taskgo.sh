586a551e77fede092e50a73e8617ac3d358d9732
yairdns <yairdns@yairdar.com>
2022-06-14 16:15:09 +0000

    fix tools upun install

--- decks/jenlib-runner-dimg/install_taskgo.sh
+++ decks/jenlib-runner-dimg/install_taskgo.sh
@@ -6,7 +6,7 @@ TASKGO_ZIPNAME=task_linux_amd64.tar.gz
 echo install task executable
 pushd /tmp
     rm -rf task_linux_amd64.tar.gz
-    wget https://github.com/go-task/task/releases/download/${TASKGO_VERSION}/${TASKGO_ZIPNAME}
+    curl -L https://github.com/go-task/task/releases/download/${TASKGO_VERSION}/${TASKGO_ZIPNAME} -o ${TASKGO_ZIPNAME}
     tar xzvf ${TASKGO_ZIPNAME}
     mv task /bin/
     rm -rf task_linux_amd64.tar.gz
--- decks/jenlib-runner-dimg/install_yq3.sh
+++ decks/jenlib-runner-dimg/install_yq3.sh
@@ -6,7 +6,7 @@ url=https://github.com/mikefarah/yq/releases/download/${version}/yq_linux_amd64
 echo install yq3 executable
 pushd /tmp
     rm -rf yq_linux_amd64
-    wget ${url}
+    curl -L ${url} -o yq_linux_amd64
     chmod +x yq_linux_amd64
     mv yq_linux_amd64 /bin/yq
     rm -rf yq_linux_amd64
